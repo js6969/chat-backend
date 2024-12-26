320bd5e54ccebcf2933cae26e02958b61b7971d2
drv44 <dhruvsarkar960@gmail.com>
2024-12-26 23:28:24 +0530

    pr

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messsageRoutes = require("./routes/messageRoutes");
 const {notFound, errorHandler} = require("./middleware/errorMiddleware");
 
 const app = express();
@@ -19,10 +19,47 @@ app.get('/', (req,res) => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
+app.use('/api/message', messsageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started successfully on PORT ${PORT}`.yellow.bold));
+const server = app.listen(PORT, console.log(`Server started successfully on PORT ${PORT}`.yellow.bold));
+
+const io = require('socket.io')(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("connected to socket.io");
+
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        socket.emit("connected");
+    });
+
+    socket.on("join chat", (room) => {
+        socket.join(room);
+        console.log("User joined room: " + room);
+    });
+
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+    socket.on("new message", (newMessageReceived) => {
+        var chat = newMessageReceived.chat;
+
+        if (!chat.users) return console.log("chat.users not defined");
+
+        chat.users.forEach(user => {
+            if (user._id === newMessageReceived.sender?._id) return;
+
+            socket.in(user._id).emit("message received", newMessageReceived);
+        });
+    });
+});
