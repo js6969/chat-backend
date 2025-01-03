02d9665701d62214f871fd061d7c9d2aa63fdfa1
drv44 <dhruvsarkar960@gmail.com>
2025-01-03 22:37:24 +0530

    p

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messsageRoutes = require("./routes/messageRoutes");
 const {notFound, errorHandler} = require("./middleware/errorMiddleware");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -13,9 +14,9 @@ const app = express();
 
 app.use(express.json());
 
-// app.get('/', (req,res) => {
-//     res.send("API is running");
-// });
+app.get('/', (req,res) => {
+    res.send("API is running");
+});
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
@@ -24,6 +25,25 @@ app.use("/api/message", messsageRoutes);
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
+
+//----------------------Deployment---------------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join( __dirname1, "frontend", "build")));
+    console.log("Frontend build directory:", path.join(__dirname1, "frontend", "build"));
+
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve( __dirname1, "frontend", "build", "index.html"));
+    });
+} else {
+    app.get('/', (req,res) => {
+        res.send("API is running");
+    });
+}
+
+//----------------------Deployment---------------------
 
 const PORT = process.env.PORT;
 
