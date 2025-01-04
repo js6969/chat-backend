99b53ff16cd9947b3473cd9487aaf302c829c690
drv44 <dhruvsarkar960@gmail.com>
2025-01-04 19:11:55 +0530

    d

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['https://js6969.github.io/chatapp']
+    origin: ['https://js6969.github.io']
 }))
 
 app.get("/", (req, res) => {
